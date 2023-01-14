c74cf987f7ee79e86e4544235e191fbd2df2e22e
Alexey Shapiro <academ@gmail.com>
2023-01-14 22:40:38 +0200

    registered apps

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'recipes.apps.UsersConfig',
+    'api.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +104,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'users.CustomUser'
