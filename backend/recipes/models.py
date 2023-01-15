7a7c32a9c57fa410adc24841a4e4fa1d4b29e1cf
Alexey Shapiro <academ@gmail.com>
2023-01-15 13:49:31 +0200

    models and migrations

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingridient(models.Model):
+    name = models.CharField(
+        max_length=250,
+        null=False,
+        blank=False,
+        verbose_name='Ingridient name'
+        )
+    measurement = models.CharField(
+        max_length=100,
+        verbose_name='Measurement unit'
+    )
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement}'
+
+    class Meta:
+        verbose_name = 'Ingridient'
+        verbose_name_plural = 'Ingridients'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'measurement'],
+                name='Unique name and measurement units combo'
+            ),
+        ]
+
+
+class Recipe(models.Model):
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Author',
+    )
+    title = models.CharField(max_length=200)
+    image = models.ImageField(
+        'Picture',
+        upload_to='recipes/',
+        blank=False,
+        null=False
+    )
+    text = models.TextField('Cooking algorithm')
+    pub_date = models.DateTimeField(
+        'Publication_date',
+        auto_now_add=True,
+        db_index=True
+    )
+    cooking_time = models.PositiveIntegerField(verbose_name='Cooking Time')
+    ingridients = models.ManyToManyField(
+        Ingridient, through='RecipeIngridients'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+        ordering = ['-pub_date']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64, verbose_name='Tag name')
+    color = models.CharField(max_length=7, verbose_name='color')
+    slug = models.SlugField(verbose_name='slug')
+    recipe = models.ManyToManyField(Recipe)
+
+
+class RecipeIngridients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingridient = models.ForeignKey(Ingridient, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(verbose_name='Ingridient amount')
+
+    def __str__(self):
+        return f'{self.ingridient} for {self.recipe}'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,37 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Follower'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Recipes author'
+    )
+
+    def __str__(self):
+        return f'{self.user} subscription on {self.author}'
+
+    class Meta:
+        verbose_name = 'Subscription'
+        verbose_name_plural = 'Subscriptions'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'], name='Unique subscription'
+            ),
+        ]
