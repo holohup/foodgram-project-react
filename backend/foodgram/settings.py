2b2b375222e5d1f20b9f8f3b7f9d6be8f8d7548b
Alexey Shapiro <academ@gmail.com>
2023-01-21 22:41:19 +0200

    some tweaks and github action

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'USER': os.getenv('PGUSER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', '5432'),
