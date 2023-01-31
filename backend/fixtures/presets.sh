2cb7b32a3d66d3a4c3249b94c61633372ad439ba
Alexey Shapiro <academ@gmail.com>
2023-01-31 17:33:08 +0200

    readme

--- backend/fixtures/presets.sh
+++ backend/fixtures/presets.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+echo "Starting process."
+echo "Preloading fixtures."
 python manage.py loaddata fixtures/presets.json
+echo "Copying images."
+if [ -d "media/recipes" ] 
+then
+    echo "Recipes directory found, copying images"
+else
+    echo "Recipes images directory not found, creating it." 
+    mkdir media/recipes
+fi
 cp fixtures/images/* media/recipes/
+echo "Presets loading complete."
