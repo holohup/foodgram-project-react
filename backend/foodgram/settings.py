a40fe1d092204f9eb9883aa0101980bacffd95e1
Alexey Shapiro <academ@gmail.com>
2023-02-06 18:08:03 +0200

    messing with static paths for tests, debug and prod

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -104,7 +104,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = (
     os.path.join(BASE_DIR, 'api/static')
-    if DEBUG
+    if DEBUG or strtobool(os.getenv('GITHUB_ACTIONS', 'False'))
     else os.path.join(BASE_DIR, 'static/django')
 )
 
