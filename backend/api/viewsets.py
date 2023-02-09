64d0b7f736aecd67e1ccd4b528824b258b4a060b
Alexey Shapiro <academ@gmail.com>
2023-02-09 15:24:07 +0200

    views.py shortification

--- backend/api/viewsets.py
+++ backend/api/viewsets.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 
 class CustomReadOnlyModelViewSet(ReadOnlyModelViewSet):
@@ -8,3 +11,31 @@ class CustomReadOnlyModelViewSet(ReadOnlyModelViewSet):
     permission_classes = (AllowAny,)
     pagination_class = None
     http_method_names = ('get',)
+
+
+class CustomModelViewsSet(ModelViewSet):
+    """Common methods for foodgram views."""
+
+    def generic_create(self, serializer, klass, outer_field):
+        """Generic create an authenticated user."""
+
+        data = {
+            outer_field: get_object_or_404(klass, id=self.kwargs['pk']).id,
+            'user': self.request.user.id,
+        }
+        serializer = serializer(data=data, context={'request': self.request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def generic_delete(self, klass, outer_klass, outer_field):
+        """Generic delete for an authenticated user."""
+
+        g_o_404_args = {
+            'user': self.request.user,
+            outer_field: get_object_or_404(outer_klass, id=self.kwargs['pk'])
+        }
+        obj = get_object_or_404(klass, **g_o_404_args)
+
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -489,7 +489,7 @@ class ShoppingCartEndpointsTests(APITestCase):
         response = self.author_client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         response = self.author_client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
